3a1fc9c21f6edf5756f28752d68bfd671affb973
slwu89 <slwu89@berkeley.edu>
2017-04-05 18:17:47 -0700

    test namespace loading

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("MASH-MBPT (Modular Analysis and Simulation for Human Health - Mosquito-borne Pathogen Transmission): version 0.9")
+}
