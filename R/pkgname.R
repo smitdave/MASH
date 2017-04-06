324beca29359773cbbab891dcc70cc5e0bf5aace
slwu89 <slwu89@berkeley.edu>
2017-04-06 09:43:38 -0700

    base pkg docs

---
+++ R/pkgname.R
@@ -0,0 +1,9 @@
+#' MASH-MBPT (Modular Analysis and Simulation for Human Health - Mosquito-borne Pathogen Transmission)
+#'
+#' MASH.MBPT is a generic, modular, and extensible modeling framework for the modeling and analysis of
+#' mosquito-borne pathogens.
+#'
+#' Please refer to \url{http://smitdave.github.io/MASH-Development/index.html} for documentation, citations, publications,
+#' acknowledgements, and examples.
+"_PACKAGE"
+#> [1] "_PACKAGE"
