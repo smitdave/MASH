d2d059a8f48d6f7401b9bba5cd3e34cb4455c054
slwu89 <slwu89@berkeley.edu>
2017-04-07 09:14:59 -0700

    test docs with LaTeX

--- R/PATHOGEN-PFSI-SetupPfSI.R
+++ R/PATHOGEN-PFSI-SetupPfSI.R
@@ -1,11 +1,39 @@
 ########################################
-#  PFSI
+#  PFSI Setup
 #  Sean Wu
 #  April 5, 2017
 ########################################
 
-
-
+#' Initialize PfSI Module Parameters
+#'
+#' Generate a list of parameters and define functions in the global environment for PfSI. All arguments have default values which are listed below before the definition.
+#'
+#' @param Pf_c 0.15; transmission efficiency: infected mosquito to human
+#' @param Pf_b 0.55; transmission efficiency: infected human to mosquito
+#' @param DurationPf 200; duration of infection (How many days does the infection last?)
+#' @param LatentPf 10; latency (How many days after the infectious bite does the infection start?)
+#' @param FeverPf 0.3; probability of fever
+#' @param mnFeverPf 10; mean of Gaussian-distributed timing of fever incident (relative to start of the infection)
+#' @param vrFeverPf 0.1; standard deviation of Gaussian-distributed timing of fever incident (relative to start of the infection)
+#' @param TreatPf 0.5; probability of treatment
+#' @param mnTreatPf 1; timing of treatment (relative to start of the fever)
+#' @param mnChemoprophylaxisPf 32; Prophylaxis, time to susceptibility (duration of protection due to chemoprophylaxis)
+#' @param PEProtectPf 0.99; proportion protected by PE vaccination (probability vaccination successful)
+#' @param peBlockPf 1; proportion of infections blocked by PE vaccination
+#' @param mnPEPf 270; mean duration of protection from PE vaccination
+#' @param vrPEPf 50; standard deviation of protection from PE vaccination
+#' @param GSProtectPf 1; proportion protected by GS vaccination (probability vaccination successful)
+#' @param gsBlockPf 0.9; proportion of infections blocked by GS vaccination
+#' @param mnGSPf 180; mean duration of protection from GS vaccination
+#' @param vrPEPf 20; standard deviation of protection from GS vaccination
+#' @param rdtSensPf 0.9; RDT sensitivity
+#' @param rdtSpecPf 0.1; RDT specificity
+#' @param lmSensPf 0.9; Light Microscopy sensitivity
+#' @param lmSpecPf 0.1; Light Microscopy specificity
+#' @param KeepPfHistory TRUE; record PfSI events in HUMANS[[ixH]]$Pathogens$Pf$eventT?
+#' @return define functions and parameters in global environment
+#' @examples
+#' PFSI.SETUP()
 PFSI.SETUP <- function(
 
     ########################################
