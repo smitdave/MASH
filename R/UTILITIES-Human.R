18f55df9c991bcf0f05aefad1bbd5c9a40d27f02
slwu89 <slwu89@berkeley.edu>
2017-04-10 18:05:30 -0700

    update tests and docs

--- R/UTILITIES-Human.R
+++ R/UTILITIES-Human.R
@@ -63,64 +63,116 @@ importHumanEvent_PfSI <- function(directory){
   return(humanHistories)
 }
 
+#' Extract PfSI History from Human Object
+#'
+#' Essentially go through the process of exporting via \code{\link{writeHumanEvent_PfSI}} and importing via \code{\link{importHumanEvent_PfSI}}
+#' without exporting data to .json to return formatted human event histories.
+#'
+#' @param HUMANS the \code{HUMANS} object
+#' @return list of histories
+#' * eventT: times events occur
+#' * events: events
+#' @md
+#' @examples
+#' writeHumanEvent(directory, fileName)
+convertHumanEvent_PfSI <- function(HUMANS){
+  humanHistories = lapply(HUMANS,function(x){
+    list(
+      eventT = x$Pathogens$Pf$eventT,
+      events = x$Pathogens$Pf$events
+    )
+  })
+  humanID = sapply(HUMANS,function(x){x$myID})
+  names(humanHistories) = paste0("human",humanID)
+  return(humanHistories)
+}
+
 
 ##########################################
 # PfSI Trajectories
 ##########################################
 
-pfsiOneTrajectory <- function(ixH){
+#' Plot a Single Human Trajectory for PfSI Module
+#'
+#' Plots a single trajectory through state space for PfSI module.
+#'
+#' @param ixH index of human
+#' @param oneHistory a single human histoy \code{history[[ixH]]}
+#' @param tMax maximum time event observed in all histories
+#' @return plot
+#' @examples
+#' plotPfsiOneTrajectory
+plotPfsiOneTrajectory <- function(ixH, oneHistory, tMax){
 
-  eT = HUMANS[[ixH]]$Pathogens$Pf$eventT[-1]
-  eV = HUMANS[[ixH]]$Pathogens$Pf$events[-1]
-  ixF = which(eV == "F") # fever
-  if(length(ixF >0)){
-    fevers = eT[ixF]
-    eT = eT[-ixF]
-    eV = eV[-ixF]
-    points(fevers, 0*fevers+ixH, pch = 17,cex = 0.5, col = "red")
+  times = oneHistory$eventT[-1]
+  events = oneHistory$events[-1]
+
+  # fever
+  ixF = which(events == "F")
+  if(length(ixF)>0){
+    points(x = times[ixF],y = rep(x = ixH,times = length(ixF)),pch=17,cex=0.5,col="red")
   }
 
-  ixP = which(eV == "P") # prophylaxis
-  if(length(ixP >0)){
-    treated = eT[ixP]
-    points(treated, 0*treated+ixH, pch = 16, cex = .5, col = "blue")
+  # prophylaxis
+  ixP = which(events == "P")
+  if(length(ixP)>0){
+    points(x = times[ixP],y = rep(x = ixH,times = length(ixP)),pch=16,cex=0.5,col="blue")
   }
 
-  if(length(eT[-1]) > 0){
-    for(i in 1:length(eT[-1])){
-      if(eV[i] == "I"){
-        segments(eT[i],ixH,eT[i+1],ixH, col = "red", lwd = 2)
+  events = events[-ixF]
+  times = times[-ixF]
+
+  # state trajectory
+  if(length(events) > 0){
+
+    for(i in 1:(length(events))){
+
+      if(i == length(events)){ # final trajectory
+        if(events[i] == "I"){
+          segments(x0 = times[i],y0 = ixH,x1 = tMax,y1 = ixH,col = "red",lwd = 2)
+        }
+        if(events[i] == "S"){
+          segments(x0 = times[i],y0 = ixH,x1 = tMax,y1 = ixH,col = "grey50",lwd = 2)
+        }
+        if(events[i] == "P"){
+          segments(x0 = times[i],y0 = ixH,x1 = tMax,y1 = ixH,col = "blue",lwd = 2)
+        }
+
+      } else { # interior trajectory
+        if(events[i] == "I"){
+          segments(x0 = times[i],y0 = ixH,x1 = times[i+1],y1 = ixH,col = "red",lwd = 2)
+        }
+        if(events[i] == "S"){
+          segments(x0 = times[i],y0 = ixH,x1 = times[i+1],y1 = ixH,col = "grey50",lwd = 2)
+        }
+        if(events[i] == "P"){
+          segments(x0 = times[i],y0 = ixH,x1 = times[i+1],y1 = ixH,col = "blue",lwd = 2)
+        }
       }
-      if(eV[i] == "S"){
-        segments(eT[i],ixH,eT[i+1],ixH, col = grey(0.5), lwd = 2)
-      }
-      if(eV[i] == "P"){
-        segments(eT[i],ixH,eT[i+1],ixH, col = "blue", lwd = 2)
-      }
-    }
-  }
+
+    } # end for
+  } # end state trajectory
 
 }
 
-pfsiTrajectory <- function(){
+#' Plot Human Trajectories from PfSI Module Histories
+#'
+#' Plots all human trajectories through state space for PfSI module.
+#'
+#' @param history imported human histories from \code{\link{importHumanEvent_PfSI}}.
+#' @return plot
+#' @examples
+#' plotPfsiOneTrajectory
+plotPfsiTrajectory <- function(history){
 
-  par(xpd = T, mar = par()$mar + c(-1,-1,-4,7)) # adjust plotting area for legend
+  tMax = max(sapply(X = history,FUN = function(x){max(x$eventT)}))
 
-  # rng = c(0,1)
-  # for(i in 1:length(HUMANS)){
-  #   rng = range(rng,HUMANS[[i]]$Pathogens$Pf$eventT[-1])
-  # }
-
-  plot(c(0,tMax), c(0.9,length(HUMANS)+0.1), type = "n", xaxt = "n", yaxt = "n", ylab = "Humans", xlab = "Time")
+  plot(1,type="n",xaxt="n",yaxt="n",ylab="Humans",xlab="Time (Years)",xlim=c(0,tMax),ylim=c(0,length(history)))
   ttMax = tMax/365
-  axis(1, c(0:ttMax)*365, c(0:ttMax))
+  axis(side = 1,at = c(0:ttMax)*365,labels = c(0:ttMax))
 
-  for(i in 1:length(HUMANS)){
-    pfsiOneTrajectory(i)
+  for(ixH in 1:length(history)){
+    plotPfsiOneTrajectory(ixH = ixH,oneHistory = history[[ixH]],tMax = tMax)
   }
 
-  legend(x = tMax+(tMax*0.06), y = (length(HUMANS)+0.1) ,legend = c("Fever","Treatment","Infected","Susceptible","Prophylaxis"),
-         pch = c(17,16,NA,NA,NA), lty = c(NA,NA,1,1,1), col = c("red", "blue", "red", grey(0.5), "blue"), bty = "n")
-
-  par(xpd = F, mar=c(5, 4, 4, 2) + 0.1)
 }
