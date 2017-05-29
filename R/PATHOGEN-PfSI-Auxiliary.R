9a1029d7058187de7bda8125e058d69527434ac4
slwu89 <slwu89@berkeley.edu>
2017-05-28 17:44:05 -0700

    PfSI occupancy vector

--- R/PATHOGEN-PfSI-Auxiliary.R
+++ R/PATHOGEN-PfSI-Auxiliary.R
@@ -39,6 +39,104 @@ PfSI.Auxiliary.Setup <- function(){
 
 
 ####################################################################################
+# PfSI history to continuous time occupancy vector
+####################################################################################
+
+#' PfSI: Calculate State Space Occupancy Vector
+#'
+#' From history output of a \code{\link{HumanPop}}, convert individual histories in to a continuous time occupany vector.
+#' The PfSI module has the following state space:
+#' * S: susceptible
+#' * I: infected
+#' * F: fever
+#' * P: chemoprophylactic protection
+#' * PEvaxx: begin PE vaccination protection
+#' * PEwane: end PE vaccination protection
+#' * GSvaxx: begin GS vaccination protection
+#' * GSwane: end GS vaccination protection
+#' @md
+#'
+#' @param history human event histories from \code{HumanPop$get_History()}
+#' @param parallel run in parallel
+#' @return list
+#' @examples
+#' util_PfSIHistory()
+#' @export
+util_PfSIHistory <- function(history, parallel = TRUE){
+
+  # number of humans
+  N = length(history)
+
+  # create time bin for each discrete event
+  eventTimes = sort(unlist(lapply(history,function(x){x$eventT})))
+  timeBins = c(min(eventTimes),Filter(f = function(y){y > min(eventTimes)},x = eventTimes))
+
+  # create empty time series (each element is slice of occupancy vector at that point in time)
+  timeSeries = lapply(X = timeBins, oneTime, female = female) # bins
+
+  initState = unique(sapply(cohortTraj,function(x){x$state[1]})) # initial state
+  if(length(initState)>1){ # sanity check
+    stop("more than one initial state")
+  }
+
+  # set initial occupancy vector
+  switch(initState,
+         "F" = for(i in 1:length(timeSeries)){timeSeries[[i]]$F <- cohortN},
+         "B" = for(i in 1:length(timeSeries)){timeSeries[[i]]$B <- cohortN},
+         "R" = for(i in 1:length(timeSeries)){timeSeries[[i]]$R <- cohortN},
+         "L" = for(i in 1:length(timeSeries)){timeSeries[[i]]$L <- cohortN},
+         "O" = for(i in 1:length(timeSeries)){timeSeries[[i]]$O <- cohortN},
+         "M" = for(i in 1:length(timeSeries)){timeSeries[[i]]$M <- cohortN},
+         "S" = for(i in 1:length(timeSeries)){timeSeries[[i]]$S <- cohortN},
+         "D" = stop("initial state should not be D")
+  )
+
+  for(ixM in 1:cohortN){ # iterate over mosquitoes
+
+    time = cohortTraj[[ixM]]$time
+    state = cohortTraj[[ixM]]$state
+    for(ixT in 2:length(time)){
+      tIter = which(timeBins >= time[ixT]) # times over which to propagate current state change
+      for(i in tIter){
+        timeSeries[[i]][[state[ixT-1]]] = timeSeries[[i]][[state[ixT-1]]] - 1 # mosy ixM no longer in state[ixT-1] in all times ixT
+        timeSeries[[i]][[state[ixT]]] = timeSeries[[i]][[state[ixT]]] + 1 # mosy ixM in state[ixT] is propagated forward
+      }
+    }
+
+  }
+
+  return(timeSeries)
+
+}
+
+
+
+#' PfSI: Empty Occupancy Vector
+#'
+#' Make an empty occupancy vector at a single time slice for the \code{PfSI} module.
+#'
+#' @param time current time slice
+#' @return list
+#' @examples
+#' util_PfSISlice(time = 0)
+util_PfSISlice <- function(time){
+  list(
+    time = time,
+    S = 0,
+    I = 0,
+    F = 0,
+    P = 0,
+    PEvaxx = 0,
+    PEwane = 0,
+    GSvaxx = 0,
+    GSwane = 0
+  )
+}
+
+
+
+
+####################################################################################
 # Plotting
 ####################################################################################
 
@@ -50,7 +148,7 @@ PfSI.Auxiliary.Setup <- function(){
 #' @return do something
 #' @examples
 #' plot_PfSI_oneTrajectory()
-plot_PfsiOneTrajectory <- function(ixH, oneHistory, tMax){
+plot_PfSIOneTrajectory <- function(ixH, oneHistory, tMax){
 
   times = oneHistory$eventT[-1]
   events = oneHistory$events[-1]
@@ -131,7 +229,7 @@ plot_PfSI <- function(history){
   axis(side = 1,at = c(0:ttMax)*365,labels = c(0:ttMax))
 
   for(ixH in 1:length(history)){
-    plot_PfsiOneTrajectory(ixH = ixH,oneHistory = history[[ixH]],tMax = tMax)
+    plot_PfSIOneTrajectory(ixH = ixH,oneHistory = history[[ixH]],tMax = tMax)
   }
 
 }
