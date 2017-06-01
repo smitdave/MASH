b0e8d66a643bc38e38a7b567ddea77cca12077f7
slwu89 <slwu89@berkeley.edu>
2017-06-01 10:31:23 -0700

    MICRO.humans.setup framework

---
+++ R/MICRO-Human-ActivitySpace.R
@@ -0,0 +1,12 @@
+#################################################################
+#
+#   MASH
+#   R6-ified
+#   MICRO: Human ActivitySpace Simulation and Methods
+#   David Smith, Hector Sanchez, Sean Wu
+#   June 1, 2017
+#
+#################################################################
+
+# NOTE: ALL OF THIS STUFF NEEDS TO GET INITIALIZED TO THE PROPER CLASSES IN
+# MICRO.Humans.Setup() akin to MACRO.Humans.Setup()
