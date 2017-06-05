b4e299b5a31575a90105d345170198341e247b84
slwu89 <slwu89@berkeley.edu>
2017-06-04 18:04:02 -0700

    kernel work

--- R/MOSQUITO-Search-MicroKernel.R
+++ R/MOSQUITO-Search-MicroKernel.R
@@ -14,8 +14,8 @@
 #' Given pointers \code{S} and \code{D} to two lists of \code{MicroSite} objects,
 #' Calculate the Euclidean distance matrix between them.
 #'
-#' @param S pointer to starting sites (for example, Landscape$get_AquaSites())
-#' @param D pointer to destination sites
+#' @param S pointer to list of starting sites (for example, Landscape$get_AquaSites())
+#' @param D pointer to list of destination sites
 #' @return matrix
 #' @export
 MicroKernel_DistanceMat <- function(S, D) {
@@ -29,4 +29,29 @@ MicroKernel_DistanceMat <- function(S, D) {
     }
   }
   return(dMat)
+}
+
+#' MICRO Search Kernels: Calculate Power Kernel
+#'
+#' Given pointers \code{S} and \code{D} to two lists of \code{MicroSite} objects,
+#' Calculate one-step Markov transition matrix between sites.
+#'
+#' @param S pointer to starting sites (for example, Landscape$get_AquaSites())
+#' @param D pointer to destination sites
+#' @param sigma a param
+#' @param eps a param
+#' @param beta a param
+#' @return matrix
+#' @export
+MicroKernel_PowerKernel <- function(S, D, sigma = 3, eps = 0.1, beta = 0){
+  dW = vapply(X = S,FUN = function(x){x$get_searchWt()},FUN.VALUE = numeric(1))
+  dS2D = MicroKernel_DistanceMat(S,D)
+  S2D = matrix(0,nrow=length(S),ncol=length(D))
+
+  for(ix in 1:length(S)){
+    allProb = dW^(-beta*dS2D[i,]) * (eps + dS2D[i,])^-sigma
+    S2D[i,] = allProb / sum(allProb)
+  }
+
+  return(S2D)
 }
